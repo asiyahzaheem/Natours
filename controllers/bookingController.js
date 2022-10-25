254e9760c9f5f2d893016c834d1628f38f1a8392
asiyahzaheem <asiyahzaheem2006@gmail.com>
2022-10-25 20:45:25 +0530

    Fixing stripe bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,14 +54,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 const createBookingCheckout = async (session) => {
-  const tour = session.data.object.client_reference_id;
-  const user = (
-    await User.findOne({ email: session.data.object.customer_email })
-  ).id;
+  console.log(session);
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   // const price = session.line_items[0].price_data.unit_amount / 100;
-  const price = session.data.object.amount_subtotal / 100;
+  const price = session.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
+
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
@@ -74,9 +74,9 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.completed') {
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
-  }
+
   res.status(200).json({ recieved: true });
 };
 
