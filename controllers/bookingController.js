34a19aa208022dbf1426074c8082a6a5e88cf11d
asiyahzaheem <asiyahzaheem2006@gmail.com>
2022-10-25 13:03:25 +0530

    Improving stripe implementation 5

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,10 +54,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
+  const tour = session.data.object.client_reference_id;
+  const user = (
+    await User.findOne({ email: session.data.object.customer_email })
+  ).id;
   // const price = session.line_items[0].price_data.unit_amount / 100;
-  const price = session.line_item_group.line_items[0].price.unit_amount / 100;
+  const price = session.data.object.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
